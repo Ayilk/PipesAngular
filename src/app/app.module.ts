73a8493b2d5e15374bf623d0c5486769d8e500cd
Ayilk <srangel.h92@gmail.com>
2022-07-28 15:45:12 -0500

    Prime Flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 
 
 
@@ -15,7 +16,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRouterModule,
-    SharedModule,   
+    SharedModule,  
+    VentasModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+
 import { NumerosComponent } from './pages/numeros/numeros.component';
 import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { BasicosComponent } from './pages/basicos/basicos.component';
@@ -22,6 +25,7 @@ import { OrdenarComponent } from './pages/ordenar/ordenar.component';
   ],
   imports: [
     CommonModule,
+    PrimeNgModule
     
   ]
 })
