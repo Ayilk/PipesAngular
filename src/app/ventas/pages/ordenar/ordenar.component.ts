7eef9d018fb978f0a66d08c50994272f0fab570c
Ayilk <srangel.h92@gmail.com>
2022-07-29 17:59:31 -0500

    Pipes personalizados: Tablas y sorts

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,16 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent  {
 
   enMayusculas: boolean = true;
+
+  ordenarPor: string = '';
+  
+  heroes: Heroe[]=[
+    {
+      nombre: "Superman",
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: "Batman",
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: "Robin",
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: "Daredevil",
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: "Linterna Verde",
+      vuela: true,
+      color: Color.verde
+    }
+  ]
+
+
   cambiar(){
     this.enMayusculas = !this.enMayusculas
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  cambiarOrden(value: string){
+    this.ordenarPor = value;
+    console.log(value)
   }
 
 }
--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -9,6 +9,8 @@ import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
 import { MayusculasPipe } from './pipes/mayusculas.pipe';
+import { VuelaPipe } from './pipes/vuela.pipe';
+import { OrdenarPipe } from './pipes/ordenar.pipe';
 
 
 
@@ -19,6 +21,8 @@ import { MayusculasPipe } from './pipes/mayusculas.pipe';
     BasicosComponent,
     OrdenarComponent,
     MayusculasPipe,
+    VuelaPipe,
+    OrdenarPipe
   ],
   exports:[
     NumerosComponent,
