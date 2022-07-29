11a10b3bd12959bfa642accb5ec414a6d8ed8ebe
Ayilk <srangel.h92@gmail.com>
2022-07-29 00:44:21 -0500

    Decimal Pipes y porcentajes

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NumerosComponent implements OnInit {
+export class NumerosComponent  {
 
-  constructor() { }
+ ventas: number = 1234567.1234;
+ porcentaje: number = 1.23;
 
-  ngOnInit(): void {
-  }
+  
 
 }
